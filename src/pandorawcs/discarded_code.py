6ab40945f5aab9878e4da54d9f44cefa88f4a7e0
rae-holcomb <raeholcomb15@gmail.com>
2024-05-09 18:05:21 -0400

    WORKING INITIAL FIT FUNCTION!!!!

--- src/pandorawcs/discarded_code.py
+++ src/pandorawcs/discarded_code.py
@@ -9,3 +9,123 @@ All functions should be dates to when they were removed from their file.
 ##############################
 
 
+    def _fit_gaussian_flux_coeff(self, std=None, source_flux=None) -> Tuple[float, float, float, float]:
+        """
+        Removed from SceneFitter() on 5/9/24
+
+        New version, does a better job fitting for the shifts.
+        For a given standard deviation, finds a constant flux multiplier for the whole image. Depends on the current psf. (Currently restricted to a pure gaussian psf.)
+        
+        Output: flux_coeff, x_shift, y_shift, rmse"""
+        if std is None:
+            std = self.initial_std
+        if source_flux is None:
+            source_flux = self.gaia_flux
+
+        # set up 2d gaussian with a set stddev
+        g1 = la.lnGaussian2DGenerator('x', 'y', stddev_x_prior=(std, 1), stddev_y_prior=(std, 1))
+        g1.fit(x=self.dx, y=self.dy, data=self.z, errors=self.zerr)#, mask=data_mask)
+
+        # now add in the gradient and fit
+        dg1 = g1.gradient
+        model = g1 + dg1
+        model.fit(x=self.dx, y=self.dy, data=self.z, errors=self.zerr)#, mask=data_mask)
+
+        print(model.mu[0])
+
+        # recover the variables of interest
+        flux_coeff = np.exp(model.mu[0])
+        xshift, yshift = model.mu[-2], model.mu[-1]
+        rmse = np.sqrt(np.mean((self.z - model.evaluate(x=self.dx, y=self.dy))**2))
+
+        return model, flux_coeff, xshift, yshift, rmse
+  
+
+    def _fit_gaussian_flux_coeff_OLD(self, std=None, source_flux=None) -> Tuple[float, float, float, float]:
+        """
+        Removed from SceneFitter() on 5/9/24
+
+                Older version. Delete later.
+        May move to general PSF version.
+        For a given standard deviation, finds a constant flux multiplier for the whole image. Depends on the current psf. (Currently restricted to a pure gaussian psf.)
+        
+        Output: flux_coeff, dx, dy, rmse"""
+        if std is None:
+            std = self.initial_std
+        if source_flux is None:
+            source_flux = self.gaia_flux
+
+        scene, dfdx, dfdy = self._get_gaussian_scene(std=std)
+        flux = np.asarray(scene.sum(axis=1)).flatten()
+        dfdx = np.array(dfdx).flatten()
+        dfdy = np.array(dfdy).flatten()
+
+        # build generator, first order polynomials in flux and shifts
+        g1 = la.Polynomial1DGenerator('flux', polyorder=1)
+        g2 = la.Polynomial1DGenerator('dfdx', polyorder=1)
+        g3 = la.Polynomial1DGenerator('dfdy', polyorder=1)
+        g = g1 + g2 + g3
+
+        # fit
+        g.fit(flux=flux, dfdx=dfdx, dfdy=dfdy, data=self.y, errors=self.yerr)
+
+        # recover the variables of interest
+        flux_coeff = g.mu[1]
+        dx, dy = g.mu[3], g.mu[5]
+        rmse = np.sqrt(np.mean((self.y - g.evaluate(flux=flux, dfdx=dfdx, dfdy=dfdy))**2))
+
+        return flux_coeff, dx, dy, rmse
+
+    def update_initial_flux_coeff_OLD(self, stds: list = np.arange(0.8, 2., 0.1), source_flux=None, plot: bool=False,) -> Tuple[float, float]:
+        """
+        Removed from SceneFitter() on 5/9/24
+
+        For a grid of standard deviations, finds the std and associated flux coefficient that best fits the data. Updates the class variables with this information."""
+        if source_flux is None:
+            source_flux = self.gaia_flux
+
+        # set up arrays
+        psfs = [None] * len(stds)
+        flux_coeffs = np.zeros_like(stds)
+        xshifts = np.zeros_like(stds)
+        yshifts = np.zeros_like(stds)
+        rmses = np.zeros_like(stds)
+
+        # loop through
+        for ind, std in enumerate(stds):
+            new_psf, flux_coeff, xshift, yshift, rmse = self._fit_gaussian_flux_coeff(std=std, source_flux=source_flux)
+            print(new_psf)
+            psfs[ind] = new_psf
+            flux_coeffs[ind] = flux_coeff
+            xshifts[ind], yshifts[ind] = xshift, yshift
+            rmses[ind] = rmse
+
+        # take the STD and the flux_coeff that minimizes the resids
+        ind = np.argmin(rmses)
+        std = stds[ind]
+        flux_coeff = flux_coeffs[ind]
+        xshift, yshift = xshifts[ind], yshifts[ind]
+        rmse = rmses[ind]
+
+        # update object values
+        self.initial_std = std   # initial estimate of the std for a purely gaussian psf
+        self.gaia_flux_coeff = flux_coeff
+        self.xshift = xshift
+        self.yshift = yshift
+
+        # plot
+        if plot:
+            fig, ax = plt.subplots(1,2, figsize=[8,3])
+            ax[0].plot(stds,rmses)
+            ax[0].set_title('RMSE')
+            ax[0].axvline(std, c='r', linestyle='--')
+            ax[0].set_xlabel('STD')
+
+            ax[1].plot(stds,flux_coeffs)
+            ax[1].set_title('Flux coefficient')
+            ax[1].axvline(std, c='r', linestyle='--')
+            ax[1].set_xlabel('STD')
+            fig.tight_layout()
+
+        return std, flux_coeff, psfs
+
--- src/pandorawcs/scene_fitter.py
+++ src/pandorawcs/scene_fitter.py
@@ -83,8 +83,13 @@ class SceneFitter():
         c = self.wcs.pixel_to_world(self.C0, self.R0)
         self.ra0, self.dec0 = c.ra.deg, c.dec.deg
 
+        # NOTE: Clean up these stddev values later!
+        
         # other attributes that will change as we fit the psf
         self.initial_std = initial_std   # initial estimate of the std for a purely gaussian psf
+        self.stddev_x = initial_std   # initial estimate of the std for a purely gaussian psf
+        self.stddev_y = initial_std   # initial estimate of the std for a purely gaussian psf
+        self.initial_std_prior = initial_std_prior
         self.initial_std_prior = initial_std_prior
         self.xshift = 0
         self.yshift = 0
@@ -238,8 +243,6 @@ class SceneFitter():
         pass
 
 
-
-
     def estimate_initial_gaussian_psf(self, stds) -> np.ndarray:
         """stds - a grid of standard deviations to test on"""
         ...
@@ -277,6 +280,11 @@ class SceneFitter():
         r, phi = np.hypot(dx, dy), np.arctan2(dy, dx)
 
         return r, phi, z, zerr, dx, dy
+
+    def model_rmse(self):
+        """Calculates the RMSE between the data and the model_image."""
+        rmse = np.sqrt(np.mean((self.y - self.model_image.ravel())**2))
+        return rmse
 
     def custom_sigma_clip() -> np.ndarray:
         """Still deciding if this lives here or in utils.py"""
@@ -323,15 +331,9 @@ class SceneFitter():
         
 
     # FUNCTIONS FOR FLUX FITTING
-
-    def _fit_gaussian_flux_coeff(self, std=None, source_flux=None) -> Tuple[float, float, float, float]:
-        """
-        New version, does a better job fitting for the shifts.
-        For a given standard deviation, finds a constant flux multiplier for the whole image. Depends on the current psf. (Currently restricted to a pure gaussian psf.)
-        
-        Output: flux_coeff, x_shift, y_shift, rmse"""
+    def do_initial_fit(self, std=None, source_flux=None, update=False):
         if std is None:
-            std = self.initial_std
+                std = self.initial_std
         if source_flux is None:
             source_flux = self.gaia_flux
 
@@ -344,98 +346,16 @@ class SceneFitter():
         model = g1 + dg1
         model.fit(x=self.dx, y=self.dy, data=self.z, errors=self.zerr)#, mask=data_mask)
 
-        print(model.mu[0])
-
-        # recover the variables of interest
-        flux_coeff = np.exp(model.mu[0])
-        xshift, yshift = model.mu[-2], model.mu[-1]
-        rmse = np.sqrt(np.mean((self.z - model.evaluate(x=self.dx, y=self.dy))**2))
-
-        return model, flux_coeff, xshift, yshift, rmse
-        
-    def _fit_gaussian_flux_coeff_OLD(self, std=None, source_flux=None) -> Tuple[float, float, float, float]:
-        """
-        Older version. Delete later.
-        May move to general PSF version.
-        For a given standard deviation, finds a constant flux multiplier for the whole image. Depends on the current psf. (Currently restricted to a pure gaussian psf.)
-        
-        Output: flux_coeff, dx, dy, rmse"""
-        if std is None:
-            std = self.initial_std
-        if source_flux is None:
-            source_flux = self.gaia_flux
-
-        scene, dfdx, dfdy = self._get_gaussian_scene(std=std)
-        flux = np.asarray(scene.sum(axis=1)).flatten()
-        dfdx = np.array(dfdx).flatten()
-        dfdy = np.array(dfdy).flatten()
-
-        # build generator, first order polynomials in flux and shifts
-        g1 = la.Polynomial1DGenerator('flux', polyorder=1)
-        g2 = la.Polynomial1DGenerator('dfdx', polyorder=1)
-        g3 = la.Polynomial1DGenerator('dfdy', polyorder=1)
-        g = g1 + g2 + g3
-
-        # fit
-        g.fit(flux=flux, dfdx=dfdx, dfdy=dfdy, data=self.y, errors=self.yerr)
-
-        # recover the variables of interest
-        flux_coeff = g.mu[1]
-        dx, dy = g.mu[3], g.mu[5]
-        rmse = np.sqrt(np.mean((self.y - g.evaluate(flux=flux, dfdx=dfdx, dfdy=dfdy))**2))
-
-        return flux_coeff, dx, dy, rmse
-
-    def update_initial_flux_coeff_OLD(self, stds: list = np.arange(0.8, 2., 0.1), source_flux=None, plot: bool=False,) -> Tuple[float, float]:
-        """For a grid of standard deviations, finds the std and associated flux coefficient that best fits the data. Updates the class variables with this information."""
-        if source_flux is None:
-            source_flux = self.gaia_flux
-
-        # set up arrays
-        psfs = [None] * len(stds)
-        flux_coeffs = np.zeros_like(stds)
-        xshifts = np.zeros_like(stds)
-        yshifts = np.zeros_like(stds)
-        rmses = np.zeros_like(stds)
-
-        # loop through
-        for ind, std in enumerate(stds):
-            new_psf, flux_coeff, xshift, yshift, rmse = self._fit_gaussian_flux_coeff(std=std, source_flux=source_flux)
-            print(new_psf)
-            psfs[ind] = new_psf
-            flux_coeffs[ind] = flux_coeff
-            xshifts[ind], yshifts[ind] = xshift, yshift
-            rmses[ind] = rmse
-
-        # take the STD and the flux_coeff that minimizes the resids
-        ind = np.argmin(rmses)
-        std = stds[ind]
-        flux_coeff = flux_coeffs[ind]
-        xshift, yshift = xshifts[ind], yshifts[ind]
-        rmse = rmses[ind]
-
-        # update object values
-        self.initial_std = std   # initial estimate of the std for a purely gaussian psf
-        self.gaia_flux_coeff = flux_coeff
-        self.xshift = xshift
-        self.yshift = yshift
-
-        # plot
-        if plot:
-            fig, ax = plt.subplots(1,2, figsize=[8,3])
-            ax[0].plot(stds,rmses)
-            ax[0].set_title('RMSE')
-            ax[0].axvline(std, c='r', linestyle='--')
-            ax[0].set_xlabel('STD')
-
-            ax[1].plot(stds,flux_coeffs)
-            ax[1].set_title('Flux coefficient')
-            ax[1].axvline(std, c='r', linestyle='--')
-            ax[1].set_xlabel('STD')
-            fig.tight_layout()
-
-        return std, flux_coeff, psfs
-     
+        if update:
+            self.update_psf(model)
+            self.gaia_flux_coeff = np.exp(model.mu[0])
+            self.stddev_x = model[0].stddev_x
+            self.stddev_y = model[0].stddev_y
+            _, self.xshift, self.yshift = model[1].fit_mu
+            
+        # return the psf
+        return model
+   
 
     def fit_flux_weights() -> np.ndarray:
         """Fits flux weights. Incorporates errors and priors."""
